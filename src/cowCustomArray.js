9980d18d2923df9c2b5e0c8c37d504e827326aa7
Semyon Suhorada <s.suhorada@dunice.net>
2021-08-13 11:02:04 +0300

    Template prepared

--- src/cowCustomArray.js
+++ src/cowCustomArray.js
@@ -1,11 +1,45 @@
 class myArray {
+  constructor() {
+    this.length = 0;
+  }
 
+  push(data) {
+
+  }
+
+  findIndexOfById(id) {
+
+  }
+
+  removeByIndex(index) {
+
+  }
 }
 
 class Farm {
+  constructor() {
+    this.cows = new myArray();
+    this.diedCows = 0;
+  }
 
+  giveBirth(parent, id, name) {
+
+  }
+
+  endLife(id) {
+
+  }
+
+  print() {
+    
+  }
 }
 
 class Cow {
-  
+  constructor(parent, id, name) {
+    this.name = name;
+    this.id = id;
+    this.parent = parent;
+    this.sex = "Female";
+  }
 }
